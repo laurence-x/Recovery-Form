cdce87a7aa1c4099f9cbd6c873d4eb5db55f98f2
laurence-x <lpemailx@gmail.com>
2023-01-17 11:07:19 +0200

    Create TpRec.ts

---
+++ TpRec.ts
@@ -0,0 +1,15 @@
+export type checksT = {
+	Em: React.RefObject<HTMLInputElement>
+	Em2: React.RefObject<HTMLInputElement>
+	ms: React.RefObject<HTMLParagraphElement>
+	iB: React.RefObject<HTMLInputElement>
+	rD: React.RefObject<HTMLDivElement>
+	nvg: (arg: string) => void
+}
+
+export type sendT = {
+	Em: React.RefObject<HTMLInputElement>
+	ms: React.RefObject<HTMLParagraphElement>
+	rD: React.RefObject<HTMLDivElement>
+	nvg: (arg: string) => void
+}
